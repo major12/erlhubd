6015f20d3605de2fa7f294bbb43bf34a8d0268f8
keymone <keymone@gmail.com>
2009-02-05 19:51:54 +0200

    spaces instead of tabs

--- src/erlhubd.erl
+++ src/erlhubd.erl
@@ -2,70 +2,70 @@
 -export([start/0, loop/1, sender/1]).
 
 start() ->
-	case tcp_server:start(?MODULE, 4111, {?MODULE, loop}) of
-		{ok, _Pid} ->
-			io:format("[M] Server started~n");
-		{error, Reason} ->
-			io:format("[M] Error starting server: ~p~n", [Reason]),
-			Wait = 10000 + random:uniform(10000),
-			io:format("[M] Retry in ~.2f seconds~n", [Wait / 1000]),
-			receive after Wait ->
-				start()
-			end
-	end.
+    case tcp_server:start(?MODULE, 4111, {?MODULE, loop}) of
+    	{ok, _Pid} ->
+    		io:format("[M] Server started~n");
+    	{error, Reason} ->
+    		io:format("[M] Error starting server: ~p~n", [Reason]),
+    		Wait = 10000 + random:uniform(10000),
+    		io:format("[M] Retry in ~.2f seconds~n", [Wait / 1000]),
+    		receive after Wait ->
+    			start()
+    		end
+    end.
 
 loop(Socket) ->
-	Sender = spawn_link(?MODULE, sender, [self()]),
-	Client = spawn_link(client, start, [self(), Sender]),
-	Sender ! {self(), Socket, Client},
-	receiver(Socket, Client).
+    Sender = spawn_link(?MODULE, sender, [self()]),
+    Client = spawn_link(client, start, [self(), Sender]),
+    Sender ! {self(), Socket, Client},
+    receiver(Socket, Client).
 
 receiver(Socket, Client) ->
-	receiver(Socket, Client, <<>>).
+    receiver(Socket, Client, <<>>).
 
 receiver(Socket, Client, Buffer) ->
-	case gen_tcp:recv(Socket, 0) of
-		{ok, Data} ->
-			{Msg, Next} = split_message(Data),
-			DataSize    = size(Data),
-			SplitSize   = size(Msg) + size(Next),
-			if
-				DataSize =:= SplitSize + 1 ->
-					Client ! {self(), <<Buffer/binary, Msg/binary>>},
-					receiver(Socket, Client, Next);
-				true ->
-					receiver(Socket, Client, <<Buffer/binary, Msg/binary>>)
-			end;
-		Error ->
-			io:format("[M] Error: ~p~n", [Error])
-	end.
+    case gen_tcp:recv(Socket, 0) of
+    	{ok, Data} ->
+    		{Msg, Next} = split_message(Data),
+    		DataSize    = size(Data),
+    		SplitSize   = size(Msg) + size(Next),
+    		if
+    			DataSize =:= SplitSize + 1 ->
+    				Client ! {self(), <<Buffer/binary, Msg/binary>>},
+    				receiver(Socket, Client, Next);
+    			true ->
+    				receiver(Socket, Client, <<Buffer/binary, Msg/binary>>)
+    		end;
+    	Error ->
+    		io:format("[M] Error: ~p~n", [Error])
+    end.
 
 sender(Receiver) ->
-	receive
-		{Receiver, Socket, Client} ->
-			sender(Socket, Client);
-		Any -> 
-			io:format("[S] Not initialized yet: ~p~n", [Any]),
-			sender(Receiver)
-	end.
+    receive
+    	{Receiver, Socket, Client} ->
+    		sender(Socket, Client);
+    	Any -> 
+    		io:format("[S] Not initialized yet: ~p~n", [Any]),
+    		sender(Receiver)
+    end.
 
 sender(Socket, Client) ->
-	receive
-		{Client, Message} ->
-			io:format("[S] Sending message: ~p~n", [Message]),
-			gen_tcp:send(Socket, Message),
-			sender(Socket, Client);
-		Any ->
-			io:format("[S] Unknown message: ~p~n", [Any]),
-			sender(Socket, Client)
-	end.
+    receive
+    	{Client, Message} ->
+    		io:format("[S] Sending message: ~p~n", [Message]),
+    		gen_tcp:send(Socket, Message),
+    		sender(Socket, Client);
+    	Any ->
+    		io:format("[S] Unknown message: ~p~n", [Any]),
+    		sender(Socket, Client)
+    end.
 
 split_message(Binary) ->
-	split_message(<<>>, Binary).
+    split_message(<<>>, Binary).
 
 split_message(First, <<$\n, Second/binary>>) ->
-	{First, Second};
+    {First, Second};
 split_message(First, <<>>) ->
-	{First, <<>>};
+    {First, <<>>};
 split_message(First, <<B:8, Second/binary>>) ->
-	split_message(<<First/binary, B:8>>, Second).
+    split_message(<<First/binary, B:8>>, Second).
