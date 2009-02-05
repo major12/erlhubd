44fa4b94df293a31d396c22e8d75d3b409cbc5a2
keymone <keymone@gmail.com>
2009-02-05 18:06:10 +0200

    server will retry to start if address in use

--- src/client.erl
+++ src/client.erl
@@ -2,16 +2,16 @@
 -export([start/2]).
 
 start(Receiver, Sender) ->
-	io:format("[C] Client connected~n"),
-	loop(Receiver, Sender).
+    io:format("[C] Client connected~n"),
+    loop(Receiver, Sender).
 
 loop(Receiver, Sender) ->
-	receive
-		{Receiver, Message} ->
-			io:format("[C] Message from receiver: ~p~n", [Message]),
-			Sender ! {self(), Message},
-			loop(Receiver, Sender);
-		Any ->
-			io:format("[C] Unknown message: ~p~n", [Any]),
-			loop(Receiver, Sender)
-	end.
+    receive
+    	{Receiver, Message} ->
+    		io:format("[C] Message from receiver: ~p~n", [Message]),
+    		Sender ! {self(), Message},
+    		loop(Receiver, Sender);
+    	Any ->
+    		io:format("[C] Unknown message: ~p~n", [Any]),
+    		loop(Receiver, Sender)
+    end.
--- src/erlhubd.erl
+++ src/erlhubd.erl
@@ -2,8 +2,17 @@
 -export([start/0, loop/1, sender/1]).
 
 start() ->
-	tcp_server:start(?MODULE, 4111, {?MODULE, loop}),
-	io:format("[M] Server started~n").
+	case tcp_server:start(?MODULE, 4111, {?MODULE, loop}) of
+		{ok, _Pid} ->
+			io:format("[M] Server started~n");
+		{error, Reason} ->
+			io:format("[M] Error starting server: ~p~n", [Reason]),
+			Wait = 10000 + random:uniform(10000),
+			io:format("[M] Retry in ~.2f seconds~n", [Wait / 1000]),
+			receive after Wait ->
+				start()
+			end
+	end.
 
 loop(Socket) ->
 	Sender = spawn_link(?MODULE, sender, [self()]),
