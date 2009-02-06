13f08c98f1ffbb30e41fe521f51fa5bbfc7ba82b
keymone <keymone@gmail.com>
2009-02-06 19:18:06 +0200

    code upgrade

--- src/erlhubd.erl
+++ src/erlhubd.erl
@@ -1,9 +1,9 @@
 -module(erlhubd).
--export([start/0, loop/1]).
+-export([start/0, upgrade/0, loop/1]).
 
 start() ->
     case tcp_server:start(?MODULE, 4111, {?MODULE, loop}) of
-      {ok, _Pid} ->
+        {ok, _Pid} ->
             io:format("[M] Server started~n");
         {error, Reason} ->
             io:format("[M] Error starting server: ~p~n", [Reason]),
@@ -13,6 +13,19 @@ start() ->
                 start()
             end
     end.
+
+upgrade() ->
+    code:purge(client),
+    code:load_file(client),
+    code:purge(client_adc),
+    code:load_file(client_adc),
+    code:purge(client_nmdc),
+    code:load_file(client_nmdc),
+    code:purge(erlhubd),
+    code:load_file(erlhubd),
+    code:purge(tcp_server),
+    code:load_file(tcp_server),
+    ok.
 
 loop(Socket) ->
     client:start(Socket).
