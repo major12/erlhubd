588d9843c4154b3404046e1127a2180e866629c3
keymone <keymone@gmail.com>
2009-02-09 20:41:23 +0200

    make ets private, access by sending messages to clients pool

--- src/clients_pool.erl
+++ src/clients_pool.erl
@@ -4,45 +4,68 @@
 -include("records.hrl").
 
 start() ->
-    Pid = spawn(?MODULE, init, []),
-    register(?MODULE, Pid),
-    {ok, Pid}.
+    case whereis(?MODULE) of
+        undefined ->
+            Pid = spawn(?MODULE, init, []),
+            register(?MODULE, Pid),
+            {ok, Pid};
+        Pid ->
+            {ok, Pid}
+    end.
 
 init() ->
-    ets:new(?MODULE, [named_table, {keypos, 3}, public, set]),
+    ets:new(?MODULE, [named_table, {keypos, 3}, private, set]),
     loop().
 
 loop() ->
     receive
+        change_code ->
+            ?MODULE:loop();
         die ->
             dead;
         {broadcast, Message} ->
             ets:foldl(fun(#client{pid=P}, ok) ->
                           P ! {Message},
                           ok
-                      end, ok, pool)
+                      end, ok, pool),
+            loop();
+        {From, add, {Pid, Nick}} ->
+            Reply = ets:insert_new(?MODULE, #client{pid=Pid, nick=Nick}),
+            From ! {self(), add, Reply},
+            loop();
+        {From, foreach, Fun} ->
+            Reply = ets:foldl(fun(E, ok) -> Fun(E), ok end, ok, clients_pool),
+            From ! {self(), foreach, Reply},
+            loop();
+        {From, update, {Nick, Field, Value}} ->
+            C = ets:lookup(clients_pool, Nick),
+            Reply = if
+                C#client.pid =:= self() ->
+                    Pos = index(Field, record_info(fields, client)),
+                    ets:update_element(clients_pool, {Pos, Value}),
+                    ok;
+                true ->
+                    {error, no_access}
+            end,
+            From ! {self(), update, Reply},
+            loop();
+        {From, delete, Nick} ->
+            Reply = ets:delete(clients_pool, Nick),
+            From ! {self(), delete, Reply},
+            loop()
     end.
 
 add(Pid, Nick) ->
-    ets:insert_new(?MODULE, #client{pid=Pid, nick=Nick}).
+    rpc(add, {Pid, Nick}).
 
 foreach(Fun) ->
-    ets:foldl(fun(E, ok) -> Fun(E), ok end, ok, clients_pool),
-    ok.
+    rpc(foreach, Fun).
 
 update(Nick, Field, Value) ->
-    C = ets:lookup(clients_pool, Nick),
-    if
-        C#client.pid =:= self() ->
-            Pos = index(Field, record_info(fields, client)),
-            ets:update_element(clients_pool, {Pos, Value}),
-            ok;
-        true ->
-            {error, no_access}
-    end.
+    rpc(update, {Nick, Field, Value}).
 
 delete(Nick) ->
-    ets:delete(clients_pool, Nick).
+    rpc(delete, Nick).
 
 index(E, L) ->
     index(E, L, 1).
@@ -53,3 +76,10 @@ index(E, [H|_], I) when E =:= H ->
     I;
 index(E, [_|T], I) ->
     index(E, T, I+1).
+
+rpc(Action, Params) ->
+    Pid = whereis(clients_pool),
+    Pid ! {self(), Action, Params},
+    receive
+        {Pid, Action, Reply} -> Reply
+    end.
