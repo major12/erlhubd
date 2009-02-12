04656b861bafea81ef417d0e215babfddb61e3c4
keymone <keymone@gmail.com>
2009-02-12 20:37:21 +0200

    role field in client record

--- src/records.hrl
+++ src/records.hrl
@@ -1,1 +1,6 @@
--record(client, {pid, nick, my_info}).
+% client
+%   pid      clients process
+%   nick
+%   my_info  temporary field for NMDC only
+%   role     can be regular, registered, vip, op, chief, admin or master
+-record(client, { pid, nick, my_info, role = regular }).
